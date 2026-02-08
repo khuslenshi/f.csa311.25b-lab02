89ebd4807a13a0930e2ca8574dc46c94911aab36
khuslenshi <kkhuslenshi@gmail.com>
2026-02-08 19:09:33 +0800

    Update Main.java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,4 @@
-package edu.cmu.cs.cs214.lab02;
+Shape rect = new Rectangle(10,5);
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
-
-public class Main {
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
-        Renderer renderer = new Renderer(rectangle);
-
-        renderer.draw();
-    }
-}
+Renderer renderer = new Renderer();
+renderer.draw(rect);
