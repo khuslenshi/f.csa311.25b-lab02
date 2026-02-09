94abcd1f58a79bf293de413d1f4b72596c9b0955
khuslenshi <kkhuslenshi@gmail.com>
2026-02-09 18:28:49 +0800

    Update Shape.java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
@@ -3,3 +3,6 @@ package shapes;
 public interface Shape {
     void draw();
 }
+// Дүрсийн ерөнхий interface тодорхойлж
+// хэрэгжүүлэлтээс (Rectangle, Circle гэх мэт)
+// хамааралгүй ашиглах боломж бүрдүүлсэн
