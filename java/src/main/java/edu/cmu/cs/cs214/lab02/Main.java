bc7a390337774a781f314c9e7fc1ef14a4d53850
khuslenshi <kkhuslenshi@gmail.com>
2026-02-09 18:45:56 +0800

    Update Main.java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -2,3 +2,5 @@ Shape rect = new Rectangle(10,5);
 
 Renderer renderer = new Renderer();
 renderer.draw(rect);
+// Main нь тодорхой дүрс (Rectangle) доторх мэдээллээс хамаарахгүй,
+// зөвхөн Shape interface ашиглан Renderer-д дамжуулж байна.
